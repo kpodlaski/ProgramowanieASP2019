1304b448d6499a0b7ec2aa15383a998f798a6b22
kpodlaski <podlaski@uni.lodz.pl>
2019-10-12 12:52:27 +0200

    Zajecia 12.10.2019

---
+++ AplikacjaPierwsza/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AplikacjaPierwsza
+{
+    class Person : IComparable<Person>
+    {
+        public String Name;
+        public String Surname;
+        public long Pesel { get; private set; }
+
+        public Person(String name, String sname, long pesel)
+        {
+            this.Name = name;
+            this.Surname = sname;
+            this.Pesel = pesel;
+        }
+
+        public override String ToString()
+        {
+            return Name + " " + Surname + " -- "+this.GetHashCode();
+        }
+
+        public int CompareTo(Person other)
+        {
+            return Math.Sign(Pesel/10000 - other.Pesel/10000);
+        }
+
+        public override int GetHashCode()
+        {
+            return (int) (Pesel / 10000 + Pesel % 10000);
+        }
+        public override bool Equals(object obj)
+        {
+            return Pesel==((Person) obj).Pesel;
+        }
+    }
+}
---
+++ AplikacjaPierwsza/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AplikacjaPierwsza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("Jan", "Kowalski", 95011285436);
+            Console.WriteLine(p);
+            Console.WriteLine(p.Pesel);
+            List<Person> persons = new List<Person>();
+            persons.Add(p);
+            persons.Add(new Person("Maria", "Mariawicka", 99052537654));
+            persons.Add(new Person("Sefania", "Stefańska", 81082537654));
+            Console.WriteLine(persons);                                 
+            persons.Sort();
+            foreach (Person p1 in persons) {
+                Console.WriteLine(p1);
+            }
+            HashSet<Person> set_persons = new HashSet<Person>(persons);
+            set_persons.Add(new Person("Jan", "Kowalski", 95011285436));
+            set_persons.Add(p);
+            Console.WriteLine(set_persons.Count);
+            foreach (Person p1 in set_persons)
+            {
+                Console.WriteLine(p1);
+            }
+        }
+    }
+}
